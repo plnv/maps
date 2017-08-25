48883f0a13fe1919aa75124fc06c07c519315d40
epalyanov <i-fly@mail.ru>
2017-08-25 09:41:54 +0300

    zoom qvan

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,3 +1,60 @@
+var key = 'AIzaSyC_Ek1yaUgPesLSbwglvjZ_TV0K-krf25E';
+
+function queryArea(item, rect) {
+  var sql = 'SELECT COUNT() FROM ' + item.id + ' WHERE ST_INTERSECTS(' + item.select + ', RECTANGLE(LATLNG(' + rect.left + '), LATLNG(' + rect.right + ')))';
+  var url = 'https://www.googleapis.com/fusiontables/v2/query?uploadType=media&alt=json&key=' + encodeURIComponent(key) + '&sql=' + encodeURIComponent(sql) + '&callback=?';
+  $.getJSON(url, function (data) {
+    console.log(data);
+    if (data.error) {
+      //alert(data.error.message);
+      return;
+    }
+    var val =  (data.rows && data.rows[0]) ? (data.rows[0] + '/') : '';
+    $('div[link=' + item.link + ']').find('.now').text(val);
+  });
+}
+
+function queryTotal(item) {
+  var sql = 'SELECT COUNT() FROM ' + item.id;
+  var url = 'https://www.googleapis.com/fusiontables/v2/query?uploadType=media&alt=json&key=' + encodeURIComponent(key) + '&sql=' + encodeURIComponent(sql) + '&callback=?';
+  $.getJSON(url, function (data) {
+    console.log(data);
+    if (data.error) {
+      // alert(data.error.message);
+      return;
+    }
+    $('div[link=' + item.link + ']').find('.total').text(data.rows[0]);
+  });
+}
+
+function getLayer(map, item) {
+  return new google.maps.FusionTablesLayer({
+    map: map,
+    heatmap: {enabled: false},
+    query: {
+      select: item.select,
+      from: item.id,
+      where: ''
+    },
+    options: {
+      styleId: 2,
+      templateId: 2
+    }
+  });
+}
+
+function getRectangel(map) {
+  var bounds = map.getBounds();
+
+  var SW = bounds.getSouthWest();
+  var left = SW.lat() + ',' + SW.lng();
+
+  var NE = bounds.getNorthEast();
+  var right = NE.lat() + ',' + NE.lng();
+
+  return {left: left, right: right};
+}
+
 function initMap() {
   google.maps.visualRefresh = true;
 
@@ -8,7 +65,6 @@ function initMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  var key = 'AIzaSyC_Ek1yaUgPesLSbwglvjZ_TV0K-krf25E';
   var layers = [
     {
       id: '1uZhN8wZRO6YTxYberuLRX-f4mkMe2rGiqGahvOcl',
@@ -31,30 +87,27 @@ function initMap() {
 
   ];
 
+  function queryAreaAll() {
+    for (var l in layers) {
+      var item = layers[l];
+      var rect = getRectangel(map);
+      queryArea(item, rect);
+    }
+  }
+
+  map.addListener('dragend', queryAreaAll);
+  map.addListener('zoom_changed', queryAreaAll);
+  setTimeout(queryAreaAll, 1000);
 
   for (var l in layers) {
     var item = layers[l];
-    item.layer = new google.maps.FusionTablesLayer({
-      map: map,
-      heatmap: {enabled: false},
-      query: {
-        select: item.select,
-        from: item.id,
-        where: ""
-      },
-      options: {
-        styleId: 2,
-        templateId: 2
-      }
-    });
-
+    item.layer = getLayer(map, item);
 
     $(function () {
       var layer = item.layer;
       var link = item.link;
 
-
-      $('.js-toggle[link=' + link + ']').on('click', function () {
+      $('div[link=' + link + '] .js-toggle').on('click', function () {
         if (layer.getMap() === null) {
           layer.setMap(map);
         } else {
@@ -62,21 +115,13 @@ function initMap() {
         }
       });
 
+      queryTotal(item);
 
-      var sql = "SELECT COUNT() FROM " + item.id;
-      var url = 'https://www.googleapis.com/fusiontables/v2/query?uploadType=media&alt=json&key=' + encodeURIComponent(key) + '&sql=' + encodeURIComponent(sql) + '&callback=?';
-      $.getJSON(url, function (data) {
-        console.log(data)
-        if (data.error) {
-          alert(data.error.message);
-          return;
-        }
-        $('.js-toggle[link=' + link + ']').parent().find(".qvan").text('(' + data.rows[0] + ')')
-      });
-
-
-    })
+    });
 
   }
+
+
+
 }
 
